20669416c933ef7418eb4cb55c70188b58e9f721
Sat178 <tobycoelen@protonmail.com>
2022-09-06 17:40:51 +0200

    Create config on start

--- UrlTok_Fixed/Program.cs
+++ UrlTok_Fixed/Program.cs
@@ -7,6 +7,7 @@ namespace UrlTok_Fixed
         [STAThread]
         private static void Main(string[] args)
         {
+            UrlTok.configCreate();
             Interface.Menu();
             Interface.Options();
             Console.ReadKey();
